1024c1c7b1dff0b8920d209fcf6ff45e4522ab06
Philip Pages <philip.pages@gmail.com>
2021-06-12 12:15:00 -0500

    most up to date rook before refactoring

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,7 +1,6 @@
 require_relative "./pieces"
 
 class Rook
-  
   include Pieces
 
   def initialize
@@ -15,44 +14,36 @@ class Rook
     elsif @board.piece_at_coordinates(@selected_piece) == "R(w)"
       @rook_color = "R(w)"
       return true
-    else
-      false
     end
   end
 
   def valid_move?
     if (@selected_piece[0] < @last[0] || @selected_piece[0] > @last[0]) && @selected_piece[1] == @last[1] ||
        (@selected_piece[1] < @last[1] || @selected_piece[1] > @last[1]) && @selected_piece[0] == @last[0]
-       
-       return true
-    else 
-      return false
+      return true
     end
   end
 
   def valid_end_coordinate?
-    if @rook_color == "R(b)" && (black_attack_white? == true || normal_move? == true)
-      return true
-    elsif @rook_color == "R(w)" && (white_attack_black? == true || normal_move? == true)
+    if @rook_color == "R(b)" && (black_attack_white? == true || normal_move? == true) ||
+       @rook_color == "R(w)" && (white_attack_black? == true || normal_move? == true)
       return true
     end
   end
 
   def rook_movement
-    
     if @last[0] > @selected_piece[0]
       rook_movement_up
     elsif @last[0] < @selected_piece[0]
       rook_movement_down
     elsif @last[1] > @selected_piece[1]
-        rook_movement_right
+      rook_movement_right
     elsif @last[1] < @selected_piece[1]
-       rook_movement_left
+      rook_movement_left
     end
   end
 
-
-  def rook_movement_checks    
+  def rook_movement_checks
     if is_rook?
       rook_movement
       if valid_end_coordinate? == true && @rook_blockage == false && valid_move? == true
@@ -65,19 +56,15 @@ class Rook
 
   private
 
-  # this just isnt working on the very @last
-
   def rook_movement_up
     @rook_blockage = false
 
-    
     until @start[0] == @last[0] - 1 || @rook_blockage == true
       @start = @start[0] + 1, @start[1]
       return @rook_blockage = true if @board.piece_at_coordinates(@start) != " .  "
     end
   end
 
-  # if the color is white and it is an attack move thenn...
   def rook_movement_down
     @rook_blockage = false
 
@@ -87,7 +74,6 @@ class Rook
     end
   end
 
-  # test for blockage on all but the last item
   def rook_movement_left
     @rook_blockage = false
 
@@ -95,20 +81,16 @@ class Rook
       @start = @start[0], @start[1] - 1
       return @rook_blockage = true if @board.piece_at_coordinates(@start) != " .  "
     end
-    
   end
 
   def rook_movement_right
     @rook_blockage = false
-    
+
     until @start[1] == @last[1] - 1 || @rook_blockage == true
       @start = @start[0], @start[1] + 1
       return @rook_blockage = true if @board.piece_at_coordinates(@start) != " .  "
     end
   end
-
-  
-
 end
 
 # rook = Rook.new
